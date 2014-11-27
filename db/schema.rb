6f6a939b49366adca7e68f7a47e2e1d0c87f37ae
rainbowaffro <akihito.tsuboi@gmail.com>
2014-11-28 00:25:00 +0900

    add schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141127141035) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "photos", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
